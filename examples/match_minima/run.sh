d1736af3153afeabb560a748153907292c8cafa3
Victoria Lim <lim.t.victoria@gmail.com>
2019-12-02 14:35:18 -0800

    organize into directory

--- examples/match_minima/run.sh
+++ examples/match_minima/run.sh
@@ -1,2 +1,1 @@
-#python ../../03_analysis/match_minima.py -i match.in --cutoff 1.0 --plot > match.out
-python ../../match_minima.py -i match.in --cutoff 1.0 --plot > match.out
+python ../../03_analysis/match_minima.py -i match.in --cutoff 1.0 --plot > match.out
